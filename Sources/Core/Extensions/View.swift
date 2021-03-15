a4a174b6efe2499094c96ac8700261be77975cdc
Vlad Kalaev <kalaevwlad@gmail.com>
2021-03-15 15:08:48 +0300

    commit

--- Sources/Core/Extensions/View.swift
+++ Sources/Core/Extensions/View.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 public struct LazyView<Content: View>: View {
-    let build: () -> Content
+    public let build: () -> Content
     
     init(_ build: @autoclosure @escaping () -> Content) {
         self.build = build
